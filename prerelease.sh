57b8baf14ac4cdf5bd34d65487309ee9f345875b
Nathan Cutler <ncutler@suse.com>
2016-03-23 11:22:55 +0100

    prerelease.sh: adapt to two significant digits in version number
    This module (Date::Holidays::CZ) uses version numbers like 0.11
    with only two significant digits.
    
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- prerelease.sh
+++ prerelease.sh
@@ -9,7 +9,7 @@ perl-reversion -bump
 perl Build.PL
 ./Build distmeta
 perl-reversion | tail -n1
-VERSION=$(grep -P 'Version \d\.*\d{3,3}' lib/Date/Holidays/CZ.pm | cut -d' ' -f2)
+VERSION=$(grep -P 'Version \d\.*\d{2,2}' lib/Date/Holidays/CZ.pm | cut -d' ' -f2)
 echo "$VERSION $(date +'%Y-%m-%d %H:%M %Z')" >>Changes
 git --no-pager log $(git describe --tags --abbrev=0)..HEAD --oneline --no-color --reverse >>Changes
 echo >>Changes
